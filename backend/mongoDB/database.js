a754b5b879532285469cf0607a98503ec9182bf5
Beheshta <beheshta.rahsepar@gmail.com>
2024-03-09 21:41:19 +0100

    update datenbank

--- backend/mongoDB/database.js
+++ backend/mongoDB/database.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://192.168.178.38:27017/TodoListDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+    console.log("Connected to MongoDB successfully!");
+});
+
+module.exports = db;
