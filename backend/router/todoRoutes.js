4dd78dfaa8c0204286ab312ee1fc0db059fcf57c
Beheshta <beheshta.rahsepar@gmail.com>
2024-03-09 23:29:33 +0100

    Add CRUD Methods to / todoRoutes.js

--- backend/router/todoRoutes.js
+++ backend/router/todoRoutes.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const todoController = require('../controllers/todoController');
+// Routes for CRUD operations
+router.post('/', todoController.createTodo);
+router.get('/', todoController.getTodos);
+router.get('/:id', todoController.getTodoById);
+router.put('/:id', todoController.updateTodo);
+router.delete('/:id', todoController.deleteTodo);
+
+module.exports = router;
